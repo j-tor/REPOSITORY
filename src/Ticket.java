7798caeacfe472939b9d7318b4ae2872e7196498
irisa <irisa@LAPTOP-P3OIAOTE>
2023-10-20 13:31:04 -0600

    ....

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,26 +1,25 @@
 
 public class Ticket {
-  
-    
+
     private String nombrePasajero;
     private double totalpagado;
-    
-    public Ticket(String nombrePasajero,double totalpagado){
-        this.nombrePasajero=nombrePasajero;
-        this.totalpagado=totalpagado;
-}
-     public String getnombrePasajero(){
+
+    public Ticket(String nombrePasajero, double totalpagado) {
+        this.nombrePasajero = nombrePasajero;
+        this.totalpagado = totalpagado;
+    }
+
+    public String getnombrePasajero() {
         return nombrePasajero;
     }
 
-    public double totalpagado(){
+    public double gettotalpagado() {
         return totalpagado;
     }
-     public String print(){
-       String print="- Nombre del Pasajero: "+this.nombrePasajero+"- Total pagado : "+this.totalpagado;
-       return print;
+
+    public String print() {
+        String print = "- Nombre del Pasajero: " + this.nombrePasajero + "- Total pagado : " + this.totalpagado;
+        return print;
     }
-    
+
 }
-
-
