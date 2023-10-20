bfaf43ba72de6036370ddd429e8a6b428481e5a0
aleja <aleja@Jeff>
2023-10-20 13:38:23 -0600

    avances

--- src/PalindromoAir.java
+++ src/PalindromoAir.java
@@ -9,4 +9,53 @@
  */
 public class PalindromoAir {
     
+    
+    public  int firstAvailable(){
+        
+        
+     return -1;
+    }
+    
+    public int searchPassenger(String name){
+    
+    return -1;
+    
+    }
+
+    
+    public boolean isPalindromo(String name){
+            return false;
+    }
+    
+    
+    public void printPassengers( ){
+    
+    }
+    
+    
+    public double income(  double ingresosGeneradosl){
+   
+    return ingresosGeneradosl;
+    }
+    
+    public void reset( ){
+    
+    }
+    
+    public void sellTicket(String name){
+    
+    }
+    
+    
+    public boolean cancelTicket(String name){
+    
+    return false;
+    }
+    
+    
+    public void dispatch( ){
+    
+    }
+    
+    
 }
